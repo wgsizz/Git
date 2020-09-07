3d28f810f64e60f845c90cd4aa5c93c12d8392f0
wgsizz <wgsizz@163.com>
2020-09-07 19:04:59 +0800

    创建工程Git-test-----222222222222222

---
+++ git-test02/src/main/java/com/bjpowernode/git/test02.java
@@ -0,0 +1,17 @@
+package com.bjpowernode.git;
+
+/**
+ * 帅帅
+ * 2020/9/7
+ */
+public class test02 {
+    public static void main(String[] args) {
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+        System.out.println("1111111111111111");
+    }
+}
